9a82fba304a0d4077132845614b526cdf49a465b
egfrank <elliot.g.frank@gmail.com>
2020-08-13 14:36:38 -0400

    Name management tests

---
+++ src/tests/test_ixctl_management.py
@@ -0,0 +1,12 @@
+def test_rsconf_generate():
+    assert 0
+
+def test_rsconf_generate_outdated():
+    assert 0
+
+
+def test_peeringdb_sync():
+    assert 0
+
+def test_peeringdb_failing_url():
+    assert 0
