123d91f0dc538e8177b38327fc40add35a458010
egfrank <elliot.g.frank@gmail.com>
2020-08-20 12:25:50 -0500

    Add invalid get endpoint tests

--- src/tests/test_ixctl_api.py
+++ src/tests/test_ixctl_api.py
@@ -39,7 +39,6 @@ def test_ix_import_peeringdb_reimport(db, pdb_data, account_objects):
     assert response.status_code == 400
     assert "You have already imported this exchange" in data["errors"]["pdb_ix_id"]
 
-
 def test_ix_list(db, pdb_data, account_objects):
 
     ix = account_objects.ix
@@ -96,6 +95,23 @@ def test_ix_create(db, pdb_data, account_objects):
     assert data[0]["name"] == "test IX new"
     assert models.InternetExchange.objects.filter(name="test IX new").exists()
 
+def test_ix_create_invalid(db, pdb_data, account_objects):
+    ix = account_objects.ix
+    client = account_objects.api_client
+    org = account_objects.org
+    
+    data = {
+        "name": None
+    }
+    response = client.post(
+        reverse("ixctl_api:ix-list", args=(org.slug,)),
+        json.dumps(data),
+        content_type="application/json"
+    )
+
+    assert response.status_code == 400
+    assert response.json()["errors"] == {"name": ["This field may not be null."]}
+
 def test_ix_members(db, pdb_data, account_objects):
     ix = account_objects.ix
     client = account_objects.api_client
@@ -138,7 +154,7 @@ def test_ix_delete_member(db, pdb_data, account_objects):
     )
 
 
-def test_ix_add_member(db, pdb_data, account_objects):
+def test_ix_create_member(db, pdb_data, account_objects):
     ix = account_objects.ix
     client = account_objects.api_client
     org = account_objects.org
@@ -166,6 +182,33 @@ def test_ix_add_member(db, pdb_data, account_objects):
     assert response.status_code == 200
 
     assert models.InternetExchangeMember.objects.filter(id=data[0]["id"]).exists()
+
+def test_ix_create_member_invalid(db, pdb_data, account_objects):
+    ix = account_objects.ix
+    client = account_objects.api_client
+    org = account_objects.org
+
+    ixmember = ix.member_set.first()
+
+    response = client.post(
+        reverse("ixctl_api:ix-members", args=(org.slug, ix.id)),
+        json.dumps(
+            {
+                "asn": 63311,
+                "ixf_state": "active",
+                "ixf_member_type": "peering",
+                "name": "",
+                "ippadr4": None,
+                "ipaddr6": ixmember.ipaddr6,
+                "speed": 1000,
+                "is_rs_peer": False,
+            }
+        ),
+        content_type="application/json",
+    )
+    errors = response.json()["errors"]
+    assert response.status_code == 400
+    assert errors == {"non_field_errors": ["IPv6 address exists already in this exchange"]}
 
 
 def test_ix_edit_member(db, pdb_data, account_objects):
@@ -201,6 +244,34 @@ def test_ix_edit_member(db, pdb_data, account_objects):
     assert ixmember.ipaddr4 == "206.41.111.20"
     assert ixmember.ipaddr6 == "2001:504:41:111::20"
 
+def test_ix_edit_member_invalid(db, pdb_data, account_objects):
+    ix = account_objects.ix
+    client = account_objects.api_client
+    org = account_objects.org
+
+    ixmember = ix.member_set.first()
+
+    response = client.put(
+        reverse("ixctl_api:ix-member", args=(org.slug, ixmember.ix.id, ixmember.id)),
+        json.dumps(
+            {
+                "id": ixmember.id,
+                "ix": ixmember.ix.id,
+                "asn": 63311,
+                "ixf_state": "active",
+                "ixf_member_type": "peering",
+                "name": "override",
+                "ipaddr4": "",
+                "ipaddr6": "",
+                "speed": 1000,
+                "is_rs_peer": False,
+            }
+        ),
+        content_type="application/json",
+    )
+    errors = response.json()["errors"]
+    assert response.status_code == 400
+    assert errors == {'ipaddr4': ['Input required for IPv4 or IPv6'], 'ipaddr6': ['Input required for IPv4 or IPv6']}
 
 def test_list_routeservers(db, pdb_data, account_objects):
     ix = account_objects.ix
@@ -311,11 +382,19 @@ def test_update_routeserver(db, pdb_data, account_objects):
     # Unchanged fields remain unchanged
     assert data[0]["ars_type"] == routeserver.ars_type
 
-# def test_retrieve_routeserverconfig():
-#     assert 0 
+def test_retrieve_routeserverconfig(db, pdb_data, account_objects):
+    rs = account_objects.routeserver
+    rsconf = rs.rsconf
+    rsconf.generate()
+    ix = account_objects.ix
+    client = account_objects.api_client
+    org = account_objects.org
 
-# def test_retrieve_routeserverconfig_plain():
-#     assert 0
+    response = client.get(reverse("ixctl_api:rsconf-detail", args=(org.slug, rs.router_id)))
+    assert response.status_code == 200
+
+    response_plain = client.get(reverse("ixctl_api:rsconf-plain", args=(org.slug, rs.router_id)))
+    assert response_plain.status_code == 200
 
 def test_list_users(db, pdb_data, account_objects):
     ix = account_objects.ix
