b5cc5d346a4dc8ed879340d5eeed3d908632152e
Elliot Frank <elliot.g.frank@gmail.com>
2021-01-26 14:55:39 -0600

    Black and isort migrations

--- src/django_ixctl/migrations/0003_internetexchange_ixf_export_privacy.py
+++ src/django_ixctl/migrations/0003_internetexchange_ixf_export_privacy.py
@@ -6,13 +6,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_ixctl', '0002_ix_slug_field'),
+        ("django_ixctl", "0002_ix_slug_field"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='internetexchange',
-            name='ixf_export_privacy',
-            field=models.CharField(choices=[('public', 'IX-F export feed is public'), ('private', 'IX-F export feed requires secret key to view')], default='public', max_length=32),
+            model_name="internetexchange",
+            name="ixf_export_privacy",
+            field=models.CharField(
+                choices=[
+                    ("public", "IX-F export feed is public"),
+                    ("private", "IX-F export feed requires secret key to view"),
+                ],
+                default="public",
+                max_length=32,
+            ),
         ),
     ]
