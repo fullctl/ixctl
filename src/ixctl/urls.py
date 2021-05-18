2f73b60a3f74397c7747ccbcdb5a2ce0a8c8c47d
Stefan Pratter <stefan@20c.com>
2021-05-18 11:26:38 +0000

    error pages

--- src/ixctl/urls.py
+++ src/ixctl/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path("", include("fullctl.django.urls")),
     path("", include("django_ixctl.urls")),
 ]
+
+handler500 = 'fullctl.django.views.handle_error_500'
+handler404 = 'fullctl.django.views.handle_error_404'
+handler403 = 'fullctl.django.views.handle_error_403'
+handler401 = 'fullctl.django.views.handle_error_401'
+handler400 = 'fullctl.django.views.handle_error_400'
