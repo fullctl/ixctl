17570b81a9ecdebbfd9854e92321c0999150e541
Matt Griswold <grizz@20c.com>
2022-11-15 05:25:10 +0000

    add options to force regen certain rs configs

--- src/django_ixctl/management/commands/ixctl_rsconf_generate.py
+++ src/django_ixctl/management/commands/ixctl_rsconf_generate.py
@@ -9,18 +9,28 @@ class Command(BaseCommand):
     Regenerate updates routeserver configs
     """
 
-    # def add_arguments(self, parser):
-    #    parser.add_argument("--pdburl", default="https://www.peeringdb.com/api")
+    def add_arguments(self, parser):
+        parser.add_argument("--only-id", help="only process this id", type=int)
+        parser.add_argument(
+            "--force", action="store_true", help="force regen of configs"
+        )
 
     def handle(self, *args, **kwargs):
+        force = kwargs["force"]
 
-        qset = Routeserver.objects.all()
+        if kwargs["only_id"]:
+            pk = kwargs["only_id"]
+            qset = Routeserver.objects.filter(id=pk)
+            print(f"only running for route server id {pk}")
+
+        else:
+            qset = Routeserver.objects.all()
 
         for rs in qset:
             routeserver_config, created = RouteserverConfig.objects.get_or_create(
                 routeserver=rs
             )
-            if created or rs.routeserver_config.outdated:
+            if force or created or rs.routeserver_config.outdated:
                 self.stdout.write(f"Regenerating {rs}")
                 rs.routeserver_config.generate()
                 self.stdout.write("Done")
