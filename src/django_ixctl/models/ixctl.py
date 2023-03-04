b77cbb679169c60af44f71cc2317bf13b4250bdf
Martin Komon <martin@mkomon.cz>
2023-03-04 20:57:05 +0100

    fix overriding of nested dicts by extra config

--- src/django_ixctl/models/ixctl.py
+++ src/django_ixctl/models/ixctl.py
@@ -4,6 +4,7 @@ import subprocess
 import tempfile
 from datetime import datetime
 from secrets import token_urlsafe
+from pydantic.utils import deep_update
 
 try:
     from yaml import CDumper as Dumper
@@ -529,10 +530,11 @@ class Routeserver(HandleRefModel):
             #
             # support both approaches for now
 
+            # use deep_update instead of update to prevent overriding of nested dicts
             if "cfg" in extra_config:
-                ars_general["cfg"].update(extra_config["cfg"])
+                ars_general["cfg"] = deep_update(ars_general["cfg"], extra_config["cfg"])
             else:
-                ars_general.update(extra_config)
+                ars_general = deep_update(ars_general, extra_config)
 
         return ars_general
 
